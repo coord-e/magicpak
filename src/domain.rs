daf9476adaff8b867c777be76a8502f830cfb031
coord.e <me@coord-e.com>
2020-03-21 05:43:20 +0000

    Change: Separate `resource` module

--- src/domain.rs
+++ src/domain.rs
@@ -2,8 +2,10 @@ pub mod bundle;
 pub mod bundle_path;
 pub mod executable;
 pub mod jail;
+pub mod resource;
 
 pub use bundle::Bundle;
 pub use bundle_path::{BundlePath, BundlePathBuf};
 pub use executable::Executable;
 pub use jail::Jail;
+pub use resource::Resource;
--- src/domain/bundle.rs
+++ src/domain/bundle.rs
@@ -4,7 +4,7 @@ use std::fs;
 use std::path::{Path, PathBuf};
 
 use crate::base::Result;
-use crate::domain::{BundlePath, BundlePathBuf, Jail};
+use crate::domain::{BundlePath, BundlePathBuf, Jail, Resource};
 
 use log::{debug, info};
 
@@ -153,49 +153,5 @@ fn sync_copy(from: &Path, to: &BundlePath, dest: &Path) -> Result<()> {
         info!("emit: copy {} => {}", from.display(), target.display());
         fs::copy(from, target)?;
         Ok(())
-    }
-}
-
-pub trait Resource {
-    fn bundle_to(self, bundle: &mut Bundle);
-}
-
-impl Resource for &Path {
-    fn bundle_to(self, bundle: &mut Bundle) {
-        bundle.add_file_from(BundlePath::projection(&self), self);
-    }
-}
-
-impl Resource for PathBuf {
-    fn bundle_to(self, bundle: &mut Bundle) {
-        self.as_path().bundle_to(bundle);
-    }
-}
-
-impl Resource for &PathBuf {
-    fn bundle_to(self, bundle: &mut Bundle) {
-        self.as_path().bundle_to(bundle);
-    }
-}
-
-impl<R> Resource for Option<R>
-where
-    R: Resource,
-{
-    fn bundle_to(self, bundle: &mut Bundle) {
-        if let Some(x) = self {
-            x.bundle_to(bundle);
-        }
-    }
-}
-
-impl<R> Resource for Vec<R>
-where
-    R: Resource,
-{
-    fn bundle_to(self, bundle: &mut Bundle) {
-        for x in self {
-            x.bundle_to(bundle);
-        }
     }
 }
