f4c1a5407498948152326e5617ecaa62d6b2203e
coord_e <me@coord-e.com>
2022-02-03 09:21:21 +0900

    refactor: Replace structopt dependency with clap 3

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,65 +4,65 @@ use magicpak::action;
 use magicpak::base::Result;
 use magicpak::domain::{Bundle, Executable};
 
+use clap::Parser;
 use log::error;
-use structopt::StructOpt;
 
-#[derive(StructOpt)]
-#[structopt(name = "magicpak")]
-struct Opt {
-    #[structopt(value_name = "INPUT", parse(from_os_str))]
+#[derive(Parser)]
+#[clap(name = "magicpak")]
+struct Args {
+    #[clap(value_name = "INPUT", parse(from_os_str))]
     /// Input executable
     input: PathBuf,
 
-    #[structopt(value_name = "OUTPUT", parse(from_os_str))]
+    #[clap(value_name = "OUTPUT", parse(from_os_str))]
     /// Output destination
     output: PathBuf,
 
-    #[structopt(short, long, value_name = "GLOB")]
+    #[clap(short, long, value_name = "GLOB")]
     /// Additionally include files/directories with glob patterns
     include: Vec<String>,
 
-    #[structopt(short, long, value_name = "GLOB")]
+    #[clap(short, long, value_name = "GLOB")]
     /// Exclude files/directories from the resulting bundle with glob patterns
     exclude: Vec<String>,
 
-    #[structopt(long, value_name = "PATH")]
+    #[clap(long, value_name = "PATH")]
     /// Make directories in the resulting bundle
     mkdir: Vec<String>,
 
-    #[structopt(short = "r", long, value_name = "PATH")]
+    #[clap(short = 'r', long, value_name = "PATH")]
     /// Specify the installation path of the executable in the bundle
     install_to: Option<String>,
 
-    #[structopt(long, value_name = "LEVEL", default_value = "Warn", possible_values = &["Off", "Error", "Warn", "Info", "Debug"])]
+    #[clap(long, value_name = "LEVEL", default_value = "Warn", possible_values = &["Off", "Error", "Warn", "Info", "Debug"])]
     /// Specify the log level
     log_level: log::LevelFilter,
 
-    #[structopt(short, long)]
+    #[clap(short, long)]
     /// Verbose mode, same as --log-level Info
     verbose: bool,
 
-    #[structopt(short, long)]
+    #[clap(short, long)]
     /// Enable testing
     test: bool,
 
-    #[structopt(long, value_name = "COMMAND")]
+    #[clap(long, value_name = "COMMAND")]
     /// Specify the test command to use in --test
     test_command: Option<String>,
 
-    #[structopt(long, value_name = "CONTENT")]
+    #[clap(long, value_name = "CONTENT")]
     /// Specify stdin content supplied to the test command in --test
     test_stdin: Option<String>,
 
-    #[structopt(long, value_name = "CONTENT")]
+    #[clap(long, value_name = "CONTENT")]
     /// Test stdout of the test command
     test_stdout: Option<String>,
 
-    #[structopt(short, long)]
+    #[clap(short, long)]
     /// Enable dynamic analysis
     dynamic: bool,
 
-    #[structopt(
+    #[clap(
         long,
         value_name = "ARG",
         allow_hyphen_values = true,
@@ -71,15 +71,15 @@ struct Opt {
     /// Specify arguments passed to the executable in --dynamic
     dynamic_arg: Vec<String>,
 
-    #[structopt(long, value_name = "CONTENT")]
+    #[clap(long, value_name = "CONTENT")]
     /// Specify stdin content supplied to the executable in --dynamic
     dynamic_stdin: Option<String>,
 
-    #[structopt(short, long)]
+    #[clap(short, long)]
     /// Compress the executable with npx
     compress: bool,
 
-    #[structopt(
+    #[clap(
         long,
         value_name = "ARG",
         allow_hyphen_values = true,
@@ -88,76 +88,76 @@ struct Opt {
     /// Specify arguments passed to upx in --compress
     upx_arg: Vec<String>,
 
-    #[structopt(long, value_name = "PATH or NAME", default_value = "busybox")]
+    #[clap(long, value_name = "PATH or NAME", default_value = "busybox")]
     /// Specify the path or name of busybox that would be used in testing
     busybox: String,
 
-    #[structopt(long, value_name = "PATH or NAME", default_value = "upx")]
+    #[clap(long, value_name = "PATH or NAME", default_value = "upx")]
     /// Specify the path or name of upx that would be used in compression
     upx: String,
 
-    #[structopt(long, value_name = "PATH or NAME", default_value = "cc", env = "CC")]
+    #[clap(long, value_name = "PATH or NAME", default_value = "cc", env = "CC")]
     /// Specify the path or name of c compiler that would be used in
     /// the name resolution of shared library dependencies
     cc: String,
 }
 
-fn run(opt: &Opt) -> Result<()> {
+fn run(args: &Args) -> Result<()> {
     let mut bundle = Bundle::new();
-    let mut exe = Executable::load(&opt.input)?;
+    let mut exe = Executable::load(&args.input)?;
 
-    action::bundle_shared_object_dependencies(&mut bundle, &exe, &opt.cc)?;
+    action::bundle_shared_object_dependencies(&mut bundle, &exe, &args.cc)?;
 
-    if opt.dynamic {
+    if args.dynamic {
         action::bundle_dynamic_dependencies(
             &mut bundle,
             &exe,
-            &opt.dynamic_arg,
-            opt.dynamic_stdin.as_ref(),
+            &args.dynamic_arg,
+            args.dynamic_stdin.as_ref(),
         )?;
     }
 
-    if opt.compress {
-        action::compress_exexcutable(&mut exe, &opt.upx, &opt.upx_arg)?;
+    if args.compress {
+        action::compress_exexcutable(&mut exe, &args.upx, &args.upx_arg)?;
     }
 
-    action::bundle_executable(&mut bundle, &exe, &opt.input, opt.install_to.as_ref())?;
+    action::bundle_executable(&mut bundle, &exe, &args.input, args.install_to.as_ref())?;
 
-    for dir in &opt.mkdir {
+    for dir in &args.mkdir {
         action::make_directory(&mut bundle, dir);
     }
 
-    for glob in &opt.include {
+    for glob in &args.include {
         action::include_glob(&mut bundle, glob)?;
     }
 
-    for glob in &opt.exclude {
+    for glob in &args.exclude {
         action::exclude_glob(&mut bundle, glob)?;
     }
 
-    if opt.test {
+    if args.test {
         action::test(
             &bundle,
             &exe,
-            opt.test_command.as_ref(),
-            opt.test_stdin.as_ref(),
-            opt.test_stdout.as_ref(),
-            &opt.busybox,
+            args.test_command.as_ref(),
+            args.test_stdin.as_ref(),
+            args.test_stdout.as_ref(),
+            &args.busybox,
         )?;
     }
 
-    action::emit(&mut bundle, &opt.output)?;
+    action::emit(&mut bundle, &args.output)?;
 
     Ok(())
 }
 
 fn main() {
-    let opt = Opt::from_args();
+    let args = Args::parse();
 
-    let log_level = if opt.verbose {
+    let log_level = if args.verbose {
         log::LevelFilter::Info
     } else {
-        opt.log_level
+        args.log_level
     };
 
     fern::Dispatch::new()
@@ -167,7 +167,7 @@ fn main() {
         .apply()
         .unwrap();
 
-    std::process::exit(match run(&opt) {
+    std::process::exit(match run(&args) {
         Ok(()) => 0,
         Err(e) => {
             error!("error: {}", e);
