5be2d3b24f039d2687b72c88869e368f2d7b1f82
coord_e <me@coord-e.com>
2022-11-23 12:08:33 +0900

    fix: enable static analysis on --include

--- src/action/include_glob.rs
+++ src/action/include_glob.rs
@@ -1,12 +1,20 @@
 use crate::base::Result;
-use crate::domain::Bundle;
+use crate::domain::{Bundle, Executable};
 
-pub fn include_glob(bundle: &mut Bundle, pattern: &str) -> Result<()> {
+pub fn include_glob(bundle: &mut Bundle, pattern: &str, cc: &str) -> Result<()> {
     tracing::info!(%pattern, "action: include using glob");
+
+    let cc_path = which::which(cc)?;
 
     for entry in glob::glob(pattern)? {
         match entry {
-            Ok(path) => bundle.add(path),
+            Ok(path) => {
+                if let Ok(obj) = Executable::load(&path) {
+                    bundle.add(obj.interpreter());
+                    bundle.add(obj.dynamic_libraries(&cc_path)?);
+                }
+                bundle.add(path);
+            }
             Err(e) => tracing::warn!(error = %e, "action: include_glob: Ignoring glob match"),
         }
     }
